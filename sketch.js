9ff8b400f1c53e26421b8eb3600fbb77c65cf9e4
Nihar1015 <69799996+Nihar1015@users.noreply.github.com>
2021-06-15 22:08:41 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,10 +1,14 @@
-var thief, police, police1, police2, police3, police4;
+var thief, police, police1, police2, police3;
 var block1,block2,block3,block4,block5,block6,block7,block8,block9,block10,block11,block12,block13,block14,block15,block16,block17,block18,block19,block20;
-var blockGroup;
+var blockGroup, policeGroup;
+var chest;
 var edges;
+var lives = 3;
 function preload(){
 police_running = loadAnimation("Spritesheets/P1.png","Spritesheets/P2.png","Spritesheets/P3.png","Spritesheets/P4.png","Spritesheets/P5.png","Spritesheets/P6.png")
 robber_running = loadAnimation("Spritesheets/R1.png","Spritesheets/R2.png","Spritesheets/R3.png", "Spritesheets/R4.png");
+chest_image = loadImage("chest.png");
+cell_img = loadImage("Cellimg.jpeg");
 }
 
 function setup(){
@@ -40,6 +44,15 @@ function setup(){
     police2 = createSprite(350,320,20,20);
     police2.addAnimation('police_running', police_running);
     police2.scale = 0.07;
+    
+    police3 = createSprite(675,50,20,20);
+    police3.addAnimation('police_running', police_running);
+    police3.scale = 0.07;
+
+    chest = createSprite(767,50,10,10);
+    chest.addImage('chest_image', chest_image);
+    chest.scale = 0.4;
+
 
     blockGroup = new Group();
     blockGroup.add(block1);
@@ -57,9 +70,16 @@ function setup(){
     blockGroup.add(block13);
     blockGroup.add(block14);
 
+    policeGroup = new Group();
+    policeGroup.add(police);
+    policeGroup.add(police1);
+    policeGroup.add(police2);
+    policeGroup.add(police3);
+
     police.velocityX = 5;
     police1.velocityX = 2;
     police2.velocityX = -7;
+    police3.velocityX = -10;
     
 
 }
@@ -109,7 +129,50 @@ function draw(){
         police2.mirrorX(-1);
     }
 
-    console.log(police2.velocityX);
+    if(round(police3.velocityX) == 10){
+        police3.mirrorX(1);
+    }
+
+    if(round(police3.velocityX) == -10){
+        police3.mirrorX(-1);
+    }
+
+    if(thief.isTouching(policeGroup) && lives > 0){
+        lives = lives - 1;
+        thief.x = 50;
+        thief.y = 760;
+    }
+
+    if(lives <= 0){
+        policeGroup.destroyEach();
+        blockGroup.destroyEach();
+        chest.destroy();
+        thief.destroy();
+        background(cell_img);
+        textSize(60);
+        fill("red");
+        text("YOU ARE CAUGHT, LOL",50,400);
+
+        
+    
+    }
+
+    if(lives == 3){
+        fill("green");
+    }
+
+    if(lives == 2){
+        fill("yellow");
+    }
+
+    if(lives <= 1){
+        fill("red");
+    }
+    textSize(20);
+    text("Lives:" + lives,670,790);
+    
+
+  
     
 
     
@@ -123,6 +186,8 @@ function draw(){
     police1.bounceOff(edges);
     police2.bounceOff(edges);
     police2.bounceOff(blockGroup);
+    police3.bounceOff(blockGroup);
+    police3.bounceOff(edges);
 
    
 
